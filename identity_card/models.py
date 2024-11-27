a8fba32f1fb2304ca688a5549c6eba5486bc1ed3
Mohtadi <mohtadi.adouani@gmail.com>
2024-11-27 14:39:22 -0500

    add sexe

--- identity_card/models.py
+++ identity_card/models.py
@@ -3,18 +3,30 @@ from django.db import models
 from datetime import datetime, timedelta
 
 class IdentityCard(models.Model):
-    __validity_years = 10
+
+    SEXE = {
+        'M': "Male",
+        'F': "Female"
+    }
+    VALIDITY_YEARS = 10
+    
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
 
     adress = models.CharField(max_length=65)
+    # random number generated for each card
     number = models.CharField(max_length=12, default=get_random_string(length=12))
+    sexe = models.CharField(
+        max_length=1,
+        choices=SEXE
+    )
     
     date_of_birth = models.DateField()
     place_of_birth = models.CharField(max_length=50)
     
     date_of_creation = models.DateField(auto_now_add=True)
-    expiry_date = models.DateField(default=datetime.now() + timedelta(days=__validity_years * 365 -1))
+    # TODO: improve expiration date calculation
+    expiry_date = models.DateField(default=datetime.now() + timedelta(days=VALIDITY_YEARS * 365 -1))
 
     def __str__(self) -> str:
         return f"{self.first_name} - {self.last_name}"

9fa9956c60590b294d7990feef52d152fa9e6538
Mohtadi <mohtadi.adouani@gmail.com>
2024-12-08 17:08:25 +0100

    add passport pre req to admin panel

--- factory/admin.py
+++ factory/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
-from .models import PreRequestIDC
+from .models import PreRequestIDC, PreRequestPassport
 
 # Register your models here.
 class PreRequestIDCAdmin(admin.ModelAdmin):
     pass
 admin.site.register(PreRequestIDC, PreRequestIDCAdmin)
+
+
+class PreRequestPassportAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(PreRequestPassport, PreRequestPassportAdmin)

29a533aa7a6aa520b702234a66893d146378c777
Mohtadi <mohtadi.adouani@gmail.com>
2024-12-08 17:13:53 +0100

    save related passport and idc to pre req

--- factory/tasks.py
+++ factory/tasks.py
@@ -8,6 +8,7 @@ def create_identity_card(idc_pre_req_pk):
     IdentityCard = apps.get_model('identity_card', 'IdentityCard')
     identity_card = IdentityCard.objects.create_from_pre_request(pre_request=idc_pre_req)
     idc_pre_req.identity_card = identity_card
+    idc_pre_req.save()
     print_identity_card.delay(identity_card.pk)
 
 @shared_task
@@ -25,6 +26,7 @@ def create_passport(passport_pre_req_pk):
     Passport = apps.get_model('identity_card', 'Passport')
     passport = Passport.objects.create_from_pre_request(pre_request=passport_pre_req)
     passport_pre_req.passport = passport
+    passport_pre_req.save()
     print_passport.delay(passport.pk)
 
 @shared_task
